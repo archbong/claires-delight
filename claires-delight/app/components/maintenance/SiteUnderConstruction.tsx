58dbd7d0c8ae576bbd0fe230a359597f657e6e44
archbong <archibong.chinonso.samuel@gmail.com>
2026-04-24 22:15:17 +0200

    added maintenance site

---
+++ claires-delight/app/components/maintenance/SiteUnderConstruction.tsx
@@ -0,0 +1,18 @@
+
+export default function SiteUnderConstruction() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f5f5f5",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "2rem",
+        color: "#333",
+      }}
+    >
+      Site Under Construction
+    </div>
+  );
+};
